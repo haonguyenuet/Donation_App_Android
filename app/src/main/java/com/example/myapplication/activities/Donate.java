7041ac3557dd7678bc444aa868a514efd1ecc7db
haoguyenuet <haonguyen.uet@gmail.com>
2021-03-23 15:39:51 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/activities/Donate.java
@@ -0,0 +1,75 @@
+package com.example.myapplication.activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
+import com.example.myapplication.R;
+import com.example.myapplication.models.Donation;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class Donate extends Base {
+
+    private Button          donateButton;
+    private RadioGroup      paymentMethod;
+    private ProgressBar     progressBar;
+    private NumberPicker    amountPicker;
+    private EditText        amountText;
+    private TextView        amountTotal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donate);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        donateButton = (Button) findViewById(R.id.donateButton);
+
+        paymentMethod = (RadioGroup)   findViewById(R.id.paymentMethod);
+        progressBar   = (ProgressBar)  findViewById(R.id.progressBar);
+        amountPicker  = (NumberPicker) findViewById(R.id.amountPicker);
+        amountText    = (EditText)     findViewById(R.id.paymentAmount);
+        amountTotal   = (TextView)     findViewById(R.id.totalSoFar);
+
+        amountPicker.setMinValue(0);
+        amountPicker.setMaxValue(1000);
+        progressBar.setMax(10000);
+        amountTotal.setText("$" + app.totalDonated);
+
+    }
+
+    public void donateButtonPressed (View view)
+    {
+        String method = paymentMethod.getCheckedRadioButtonId() == R.id.PayPal ? "PayPal" : "Direct";
+        int donatedAmount =  amountPicker.getValue();
+        if (donatedAmount == 0)
+        {
+            String text = amountText.getText().toString();
+            if (!text.equals(""))
+                donatedAmount = Integer.parseInt(text);
+        }
+        if (donatedAmount > 0)
+        {
+            app.newDonation(new Donation(donatedAmount, method));
+            progressBar.setProgress(app.totalDonated);
+            String totalDonatedStr = "$" + app.totalDonated;
+            amountTotal.setText(totalDonatedStr);
+        }
+    }
+
+    @Override
+    public void reset(MenuItem item)
+    {
+        app.dbManager.reset();
+        app.totalDonated = 0;
+        amountTotal.setText("$" + app.totalDonated);
+    }
+}
